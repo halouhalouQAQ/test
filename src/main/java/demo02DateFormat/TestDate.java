373433085720713332e58bc9d0e7d4f241463982
HanDongdong <738641868@qq.com>
2020-04-02 20:13:03 +0800

    test111

--- src/main/java/demo02DateFormat/TestDate.java
+++ src/main/java/demo02DateFormat/TestDate.java
@@ -3,18 +3,15 @@ package demo02DateFormat;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class TestDate {
 
     public static void main(String[] args) throws ParseException {
-        Date date = new Date();
-        System.out.println(date.toString());
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        Date parse = simpleDateFormat.parse("2019-10-22 22:11:11");
-
-        String format = simpleDateFormat.format(new Date());
-        System.out.println(format);
+        System.out.println("dododododo");
     }
 }
